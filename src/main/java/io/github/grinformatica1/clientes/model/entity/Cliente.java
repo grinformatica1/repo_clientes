7397c4ecb94bbeebd3f48bc25de1d1620d250e08
Guilherme Maciel - TI - IPOG Goiânia <guilherme.maciel@ipog.local>
2024-02-09 17:53:25 -0300

    Primeiro commit

---
+++ src/main/java/io/github/grinformatica1/clientes/model/entity/Cliente.java
@@ -0,0 +1,31 @@
+package io.github.grinformatica1.clientes.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column(name = "data_cadastro")
+    private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+    }
+}
---
+++ src/main/java/io/github/grinformatica1/clientes/rest/ClienteController.java
@@ -0,0 +1,16 @@
+package io.github.grinformatica1.clientes.rest;
+
+import io.github.grinformatica1.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteController {
+
+    private ClienteRepository repository;
+
+    @Autowired
+    public ClienteController(ClienteRepository repository){
+        this.repository = repository;
+    }
+}
